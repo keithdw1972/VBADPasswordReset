9efbc328cbce0ef5f034640c87e840f5782c9147
Keith Williams <2156280+keithdw1972@users.noreply.github.com>
2021-09-23 16:32:29 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOldPassword = New System.Windows.Forms.Label()
+        Me.lblNewPassword = New System.Windows.Forms.Label()
+        Me.txtOldPassword = New System.Windows.Forms.TextBox()
+        Me.txtNewPassword1 = New System.Windows.Forms.TextBox()
+        Me.btnResetPassword = New System.Windows.Forms.Button()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNewPassword2 = New System.Windows.Forms.TextBox()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.lblSuccess = New System.Windows.Forms.Label()
+        Me.lblPasswdError = New System.Windows.Forms.Label()
+        Me.lblCatch = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblOldPassword
+        '
+        Me.lblOldPassword.AutoSize = True
+        Me.lblOldPassword.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblOldPassword.Location = New System.Drawing.Point(68, 54)
+        Me.lblOldPassword.Name = "lblOldPassword"
+        Me.lblOldPassword.Size = New System.Drawing.Size(72, 13)
+        Me.lblOldPassword.TabIndex = 7
+        Me.lblOldPassword.Text = "Old Password"
+        Me.lblOldPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblNewPassword
+        '
+        Me.lblNewPassword.AutoSize = True
+        Me.lblNewPassword.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblNewPassword.Location = New System.Drawing.Point(62, 81)
+        Me.lblNewPassword.Name = "lblNewPassword"
+        Me.lblNewPassword.Size = New System.Drawing.Size(78, 13)
+        Me.lblNewPassword.TabIndex = 8
+        Me.lblNewPassword.Text = "New Password"
+        Me.lblNewPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtOldPassword
+        '
+        Me.txtOldPassword.Location = New System.Drawing.Point(142, 50)
+        Me.txtOldPassword.Name = "txtOldPassword"
+        Me.txtOldPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtOldPassword.TabIndex = 2
+        Me.txtOldPassword.UseSystemPasswordChar = True
+        '
+        'txtNewPassword1
+        '
+        Me.txtNewPassword1.Location = New System.Drawing.Point(142, 77)
+        Me.txtNewPassword1.Name = "txtNewPassword1"
+        Me.txtNewPassword1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNewPassword1.TabIndex = 3
+        Me.txtNewPassword1.UseSystemPasswordChar = True
+        '
+        'btnResetPassword
+        '
+        Me.btnResetPassword.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnResetPassword.Location = New System.Drawing.Point(142, 142)
+        Me.btnResetPassword.Name = "btnResetPassword"
+        Me.btnResetPassword.Size = New System.Drawing.Size(100, 23)
+        Me.btnResetPassword.TabIndex = 5
+        Me.btnResetPassword.Text = "Reset Password"
+        Me.btnResetPassword.UseVisualStyleBackColor = True
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblUsername.Location = New System.Drawing.Point(85, 28)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(55, 13)
+        Me.lblUsername.TabIndex = 6
+        Me.lblUsername.Text = "Username"
+        Me.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(142, 24)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Repeat New Password"
+        '
+        'txtNewPassword2
+        '
+        Me.txtNewPassword2.Location = New System.Drawing.Point(142, 104)
+        Me.txtNewPassword2.Name = "txtNewPassword2"
+        Me.txtNewPassword2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNewPassword2.TabIndex = 4
+        Me.txtNewPassword2.UseSystemPasswordChar = True
+        '
+        'lblError
+        '
+        Me.lblError.AutoSize = True
+        Me.lblError.Location = New System.Drawing.Point(255, 30)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(39, 13)
+        Me.lblError.TabIndex = 10
+        Me.lblError.Text = "Label2"
+        '
+        'lblSuccess
+        '
+        Me.lblSuccess.AutoSize = True
+        Me.lblSuccess.Location = New System.Drawing.Point(255, 56)
+        Me.lblSuccess.Name = "lblSuccess"
+        Me.lblSuccess.Size = New System.Drawing.Size(39, 13)
+        Me.lblSuccess.TabIndex = 11
+        Me.lblSuccess.Text = "Label3"
+        '
+        'lblPasswdError
+        '
+        Me.lblPasswdError.AutoSize = True
+        Me.lblPasswdError.Location = New System.Drawing.Point(255, 81)
+        Me.lblPasswdError.Name = "lblPasswdError"
+        Me.lblPasswdError.Size = New System.Drawing.Size(39, 13)
+        Me.lblPasswdError.TabIndex = 12
+        Me.lblPasswdError.Text = "Label4"
+        '
+        'lblCatch
+        '
+        Me.lblCatch.AutoSize = True
+        Me.lblCatch.Location = New System.Drawing.Point(255, 108)
+        Me.lblCatch.Name = "lblCatch"
+        Me.lblCatch.Size = New System.Drawing.Size(39, 13)
+        Me.lblCatch.TabIndex = 13
+        Me.lblCatch.Text = "Label5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1050, 173)
+        Me.Controls.Add(Me.lblCatch)
+        Me.Controls.Add(Me.lblPasswdError)
+        Me.Controls.Add(Me.lblSuccess)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.txtNewPassword2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.btnResetPassword)
+        Me.Controls.Add(Me.txtNewPassword1)
+        Me.Controls.Add(Me.txtOldPassword)
+        Me.Controls.Add(Me.lblNewPassword)
+        Me.Controls.Add(Me.lblOldPassword)
+        Me.Name = "Form1"
+        Me.Text = "Password Reset"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblOldPassword As Label
+    Friend WithEvents lblNewPassword As Label
+    Friend WithEvents txtOldPassword As TextBox
+    Friend WithEvents txtNewPassword1 As TextBox
+    Friend WithEvents btnResetPassword As Button
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNewPassword2 As TextBox
+    Friend WithEvents lblSuccess As Label
+    Friend WithEvents lblPasswdError As Label
+    Friend WithEvents lblCatch As Label
+    Friend WithEvents lblError As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblOldPassword.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles lblUsername.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtNewPassword1.TextChanged
+
+    End Sub
+
+    Private Sub BtnResetPassword_Click(sender As Object, e As EventArgs) Handles btnResetPassword.Click
+        lblError.Visible = False
+        lblSuccess.Visible = False
+        lblPasswdError.Visible = False
+        lblCatch.Visible = False
+        'Dim defaultNamingContext As String
+
+        If txtUsername.Text = "" Then
+            MsgBox("Please Enter Username!", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+        If txtOldPassword.Text = "" Then
+            MsgBox("Please Enter Current / Old Password!", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+        If txtNewPassword1.Text = "" Then
+            MsgBox("Please Enter New Password!", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+        If txtNewPassword2.Text = "" Then
+            MsgBox("Please Confirm New Password!", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        ElseIf txtNewPassword1.Text <> txtNewPassword2.Text Then
+            MsgBox("Passwords must match, please try again.", MsgBoxStyle.Critical, "Error")
+            lblPasswdError.Visible = True
+            Exit Sub
+        End If
+
+        Using RootDSE As New DirectoryServices.DirectoryEntry("LDAP://RootDSE")
+            Dim DomainDN As String = RootDSE.Properties("DefaultNamingContext").Value
+            Dim parts = New System.DirectoryServices.DirectoryEntry("LDAP://CN=Partitions,CN=Configuration," & DomainDN)
+            Dim NTBIOSName As String = Nothing
+            For Each part In parts.Children
+                If part.Properties("nCName")(0) = DomainDN Then
+                    'MsgBox(part.Properties("nETBIOSName")(0))
+                    NTBIOSName = part.Properties("nETBIOSName")(0)
+                    MsgBox(NTBIOSName)
+                    Exit For
+                End If
+            Next
+            Dim oldpassword As String = txtOldPassword.Text
+            Dim username As String = txtUsername.Text
+            MsgBox(DomainDN)
+            'Dim ADEntry As New System.DirectoryServices.DirectoryEntry("LDAP://" & DomainDN)
+            'Dim ADSearch As New System.DirectoryServices.DirectorySearcher(ADEntry)
+            'Dim ADSearchResult As System.DirectoryServices.SearchResult
+            '
+            'ADSearch.Filter = "(samAccountName=" & Security.Principal.WindowsIdentity.GetCurrent.Name.Split("\"c)(1) & ")"
+            'ADSearch.SearchScope = SearchScope.Subtree
+
+            'ADSearchResult = ADSearch.FindOne()
+            MsgBox($"{NTBIOSName}\{username}", oldpassword)
+            Dim ADEntry As New System.DirectoryServices.DirectoryEntry("LDAP://OU=Test," & DomainDN,
+                                                                       NTBIOSName & "\" & username, oldpassword)
+
+            Try
+                MsgBox("Try!")
+                Dim ADSearch As New System.DirectoryServices.DirectorySearcher(ADEntry) With {
+                    .SearchScope = DirectoryServices.SearchScope.Subtree,
+                    .Filter = "(SAMAccountName=" & username & ")"
+                }
+                Dim ADSearchResult As System.DirectoryServices.SearchResult
+                Dim searchResult As DirectoryServices.SearchResult = ADSearch.FindOne()
+                ADSearchResult = searchResult
+
+                Dim check As String = CType(ADSearchResult.Properties("SAMAccountName")(0), String)
+                If check = Nothing Then MsgBox("Check is nothing!")
+                If check <> Nothing Then
+                    MsgBox("Check is not nothing!")
+                    If check = username Then
+                        MsgBox("Check is username!" & check & username)
+                        Dim newpassword1 As String = txtNewPassword1.Text
+                        Dim newpassword2 As String = txtNewPassword2.Text
+                        If newpassword1 = newpassword2 Then
+                            MsgBox("Usernames match!" & oldpassword & newpassword1 & newpassword2)
+                            ADEntry = ADSearchResult.GetDirectoryEntry()
+                            ADEntry.Username = NTBIOSName & "\" & check
+                            MsgBox(ADEntry.Username)
+                            ADEntry.Password = oldpassword
+                            ADEntry.AuthenticationType = DirectoryServices.AuthenticationTypes.Secure
+                            ADEntry.Options.Referral = DirectoryServices.ReferralChasingOption.All
+                            ADEntry.Invoke("ChangePassword", oldpassword, newpassword1)
+                            ADEntry.CommitChanges()
+                            lblSuccess.Visible = "Password Changed."
+                        Else
+                            lblPasswdError.Visible = True
+                        End If
+                    Else
+                        lblError.Visible = True
+                    End If
+                Else
+                    lblError.Visible = True
+                End If
+
+                'If Not IsNothing(ADSearchResult) Then
+                'MsgBox("Hello!")
+                'MsgBox(DomainDN)
+                'MsgBox(username)
+                'MsgBox(oldpassword)
+                'MsgBox(newpassword1)
+
+                'Dim myUser As System.DirectoryServices.DirectoryEntry = New System.DirectoryServices.DirectoryEntry()
+                'myUser.Path = ADSearchResult.GetDirectoryEntry().Path
+                'myUser.AuthenticationType = System.DirectoryServices.AuthenticationTypes.Secure
+                'Dim ret As Object = myUser.Invoke("ChangePassword", oldpassword, newpassword1)
+                'myUser.CommitChanges()
+                'myUser.Close()
+
+                'newpassword1 = True
+
+                MsgBox("Password Changed.", MsgBoxStyle.Information, "Password Changed")
+
+                Me.Close()
+                'End If
+                'End Using
+            Catch ex As Exception
+                MsgBox("Fails 3!")
+                lblCatch.Text = "Error message: " + ex.InnerException.Message
+                lblCatch.Visible = True
+            End Try
+        End Using
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtOldPassword.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click_2(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles txtNewPassword2.TextChanged
+
+    End Sub
+End Class
